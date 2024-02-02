449427b5ce635e81aeae0885721abe75aa28ff58
jrnecki <129943670+jrnecki@users.noreply.github.com>
2024-02-02 09:54:38 +0900

    chore : 초기세팅
    spring boot 3.1.8
    java 17
    build.gradle 8.5

---
+++ src/main/java/com/example/artfriendly/ArtfriendlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.artfriendly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtfriendlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtfriendlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artfriendly/ArtfriendlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artfriendly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtfriendlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
